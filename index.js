3dddec4b4d06c1c7e170429ffac513cb580d84eb
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-03-02 21:05:32 +0800

    Optimizes js scripts

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const firstName = document.querySelector("#first-name");
     firstName.addEventListener("blur", validateInputField);
+    // Note: mousedown event listener is added later in code
+    //  to firstName, lastName, email, phoneNumber
 const lastName = document.querySelector("#last-name");
     lastName.addEventListener("blur", validateInputField);
 const email = document.querySelector("#email");
@@ -15,13 +17,10 @@ const confirmPassword = document.querySelector("#confirm-password");
     confirmPassword.addEventListener("input", validateConfirmPassword);
     confirmPassword.addEventListener("blur", validateConfirmPassword);
 
-
 const form = document.querySelector("form.create-account");
     form.addEventListener("submit", submitForm);
 
-
-
-let overAllValidation = [];
+let overAllValidation = []; // Saves status of input fields
 function CheckUserAction(name, viewed) {
     this.name = name;
     this.viewed = viewed;
@@ -35,8 +34,8 @@ function CheckUserAction(name, viewed) {
     let passwordStatus = new CheckUserAction("password", false);
     let confirmPasswordStatus = new CheckUserAction("confirmPassword", false);
 
-
-// validates Other Input (start) -----------------
+// Validates Other Input (start) -----------------
+//  Clears styling upon click to input field (UI related) 
 function clearStyling() {
     if (this.hasAttribute("class")) {
          this.removeAttribute("class")
@@ -46,20 +45,17 @@ function clearStyling() {
     return;
     }
 
-
 function validateInputField() {
-
 //Gets the input element id as string and 
 //  checks what input field is being validated
     let inputField = `${"#" + this.id}`;  // Id Selector String
-    let inputElement = document.querySelector(`${inputField}`);
-    let inputFieldName = [];
+    let inputElement = document.querySelector(`${inputField}`); //(<input>)
+    let inputFieldName = []; // Saves the input field currently active
 
     let fieldValue = this.value.trim(); // Ensures no whitespace
     
     let errorList = document.querySelector(`${inputField} + ul`);
     let errors = [];
-
 
 // Validation Starts Here
 // Validates First Name Input Field
@@ -103,10 +99,9 @@ if (inputField === "#email") {
         if (fieldValue.length != 0) {
             errors.push("Kindly enter a valid email address");
         } 
-        errors.push("Make sure it's written like example@email.com")
+        errors.push("Reminder: Write it like example@email.com")
     }
 }
-
 
 // Validates phone Input Field
 if (inputField === "#phone") {
@@ -128,9 +123,9 @@ if (inputField === "#phone") {
     }
 }
 
+// Checks if there are any Errors
 
-// Removes error list
-    function removeErrorList(field) {
+    function removeErrorList(field) { //  (Reusable Function)
         let newErrorList = document.querySelector(`${field} + ul`);
         let errorListNodes = newErrorList.querySelectorAll("li");
         
@@ -141,16 +136,14 @@ if (inputField === "#phone") {
         }
     }
 
-// Checks if there are any Errors
     if (errors.length > 0) {
-    // puts the error Array of string in the error list element
-        //removes error list from previous errors first
-        removeErrorList(inputField); 
+    // Puts the error Array of string in the error list element(ul > li)
+        removeErrorList(inputField); //removes error list from previous errors first
 
         errors.forEach(message => {
             let errorMessage = document.createElement("li");
             errorMessage.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> ${message}`
-            // errorMessage.textContent = message;
+
             if (errorMessage.textContent.includes("Reminder")) {
                 errorMessage.innerHTML = `<i class="fa-solid fa-circle-exclamation"></i> ${message}`
                 errorMessage.classList.add("reminder");
@@ -161,14 +154,13 @@ if (inputField === "#phone") {
         });
 
         if (inputElement.hasAttribute("class")) {
-        // Ensures that there's no current styling then 
+        // If statement ensures that there's no current styling then 
         //  add corresponding class for styling
             inputElement.removeAttribute("class");
         }
         inputElement.classList.add("invalid");
-        inputFieldName[0]["validity"] = false;
+        inputFieldName[0]["validity"] = false; // Saves validity to global object
         
-
     } else if (errors.length === 0) {
         removeErrorList(inputField);
 
@@ -178,9 +170,7 @@ if (inputField === "#phone") {
             inputElement.removeAttribute("class");
         }
         inputElement.classList.add("valid");
-        inputFieldName[0]["validity"] = true;
-        
-        
+        inputFieldName[0]["validity"] = true; // Saves validity to global object
     }
 
     // Adds active input listening that guides user if 
@@ -198,15 +188,7 @@ console.log(overAllValidation);
 // validates Other Input (end) -----------------
 
 
-
-
-
-
-
-
-
 // Validates Password (start) -----------
-
 let listItemArray = [];
 function ListItem(listElement, listIcon) {
     this.listElement = listElement;
@@ -237,7 +219,7 @@ function validatePassword(event) {
         }
     }
 
-     function promptValidity(validity,object) {
+     function promptValidity(validity,object) { // Reusable function
          if (validity === true) {
             object["listIcon"].removeAttribute("class");
             object["listIcon"].classList.add("fa-regular", "fa-circle-check");
@@ -304,18 +286,19 @@ function validatePassword(event) {
     }
 
 // Check if password is at least 8 characters
-if (/.{8,}/.test(this.value)) {
-    promptValidity(true, lengthCheck);
-} else {
-    if (event.type === "input" || event.type === "mousedown") {
-        promptValidity("neutral", lengthCheck);
-    } else if (event.type === "blur") {
-        promptValidity(false, lengthCheck);
+    if (/.{8,}/.test(this.value)) {
+        promptValidity(true, lengthCheck);
+    } else {
+        if (event.type === "input" || event.type === "mousedown") {
+            promptValidity("neutral", lengthCheck);
+        } else if (event.type === "blur") {
+            promptValidity(false, lengthCheck);
+        }
     }
-}
 
+// Checks overall validity of password
     if (event.type === "blur") {
-        let resultsArray = [];
+        let resultsArray = []; // Array of results derived from object
 
         listItemArray.forEach(item => {
             resultsArray.push(item["passed"]);
@@ -323,29 +306,31 @@ if (/.{8,}/.test(this.value)) {
     
         if (resultsArray.includes(false) || resultsArray.includes("neutral")) {
             this.classList.add("invalid");
-            passwordStatus["validity"] = false;
+            passwordStatus["validity"] = false; // Saves validity to global object
         } else {
             this.classList.add("valid");
             guideList.style.display = "none";
-            passwordStatus["validity"] = true;
+            passwordStatus["validity"] = true;  // Saves validity to global object
         }
-        console.log(resultsArray);
     }
     
     console.log(overAllValidation);
 }
 // Validates Password (end) -----------
 
-
-
 // Validates Confirm Password (start) -----------
 let confirmInputElement = document.querySelector("[data-check='confirm']");
 function validateConfirmPassword(event) {
-    let errors = [];
     let passwordInput = password.value;
 
+    //Add event listener to actively listens to the password input
     if (event.type === "mousedown") {
         password.addEventListener("input", activePassListening);
+
+        if (this.hasAttribute("class")) {
+            this.removeAttribute("class")
+       }  
+       this.nextElementSibling.style.display = "none";
     }
 
     if (event.type === "blur") {
@@ -353,68 +338,72 @@ function validateConfirmPassword(event) {
             this.removeAttribute("class")
         }
 
+        function validate(validity) { // Reusable function
+            if (validity === false) {
+                this.classList.add("invalid");
+                confirmPasswordStatus["validity"] = false;
+                this.nextElementSibling.style.display = "unset";
+            } else if (validity === true) {  
+                this.classList.add("valid");
+                confirmPasswordStatus["validity"] = true;
+            }
+        }
+
         if (this.value != passwordInput && this.value.length != 0) {
             confirmInputElement.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Must match passwords`;
-            this.classList.add("invalid");
-            confirmPasswordStatus["validity"] = false;
+            validate(false);
         } else if (this.value.length === 0 ) {
             confirmInputElement.innerHTML = `<i class="fa-regular fa-circle-xmark"></i> Enter password`;
-            this.classList.add("invalid");
-            confirmPasswordStatus["validity"] = false;
+            validate(false);
         }else if (this.value.length != 0 && this.value === passwordInput) {
             confirmInputElement.innerHTML = "";
-            this.classList.add("valid");
-            confirmPasswordStatus["validity"] = true;
+            validate(true);
         }
+        console.log(this);
     }
 
-
 function activePassListening() {
-        
-    if (confirmPassword.value != password.value) {
+    // Removes confirm pass UI, when password is altered
+    if (confirmPassword.value != passwordInput) {
         if (confirmPassword.hasAttribute("class")) {
             confirmPassword.removeAttribute("class")
          }
-        
         confirmInputElement.innerHTML = "";
     }
-
     // No UI changes when password matches during active input, 
     //  so that the user will try to confirm password again
 }
-
 console.log(overAllValidation);
 }
 // Validates Confirm Password (end) -----------
 
 
-
+// Validate and Submit (start) ----------------
 function submitForm(event) {
-    event.preventDefault();
-
-    const mouseDownEvent = new MouseEvent('mousedown', {
+    // Construct mouse event to simulates blur event when submit button is clicked.
+    // Created to be used for validation on submit button click 
+    const blurEvent = new MouseEvent('blur', {
         bubbles: true,
         cancelable: true,
         view: window
-      });
-
-      const blurEvent = new MouseEvent('blur', {
-        bubbles: true,
-        cancelable: true,
-        view: window
-      });
+    });
 
     let inputs = document.querySelectorAll("form.create-account input");
 
+    // Simulates blur event to all input fields to validate input values
     inputs.forEach(input => {
         input.dispatchEvent(blurEvent);
-    })
+    });
 
-
-    
+    // Checks the saved validations if all inputs values has passed
+    overAllValidation.forEach(validation => {
+        if (validation["validity"] === false) {
+            event.preventDefault();
+            return;
+        }
+    });
 }
-
-
+// Validate and Submit (end) ----------------
 
 
 
